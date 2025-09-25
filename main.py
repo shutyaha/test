f87e6979d6e19a62bf202f8beedd17008fe1ba6d
SVladimirWin <shutyaha@gmail.com>
2025-09-25 11:55:47 +0300

    add goodbye function

--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print('Hello_world')
-
-
+def goodbye() -> None:
+    print("Goodbye!")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
